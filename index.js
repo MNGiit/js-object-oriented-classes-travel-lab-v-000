24416f2e525b52e688e6084958b422a0516e0858
Mohammed Noori <kmanoorib@gmail.com>
2019-02-28 00:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ class Driver {
     this.name = name;
     this.startDate = new Date();
   }
+  yearsExperienceFromBeginningOf(year) {
+    
+  }
 }
+
+
+//calculates the number of years driven given an endDate ‣
+//TypeError: driver.yearsExperienceFromBeginningOf is not a function
+//    at Context.<anonymous> (test/indexTest.js:21:21)expect(driver.yearsExperienceFromBeginningOf//('2017')).to.equal(22);
