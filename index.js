a82488ed671cb724e5be6faae298e119de05ede0
Mohammed Noori <kmanoorib@gmail.com>
2019-02-28 00:39:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver {
     this.startDate = new Date();
   }
   yearsExperienceFromBeginningOf(year) {
-    return year - this.startDate.getFullYear();
+    return this.startDate.getFullYear() - year;
   }
 }
 
